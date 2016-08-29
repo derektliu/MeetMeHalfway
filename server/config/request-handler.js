f804fb228992f7ab5892b029872584104ce111b4
Derek Liu <liu.derekt@gmail.com>
2016-08-29 15:45:02 -0700

    Add routes at server for get/post requests from client.

--- server/config/request-handler.js
+++ server/config/request-handler.js
@@ -1,3 +1,14 @@
 exports.helloWorld = function(req, res) {
   res.send('Testing 1 2 3');
 };
+
+exports.requestYelp = function(req, res) {
+  var obj = { name: 'Yelp' };
+  res.send('SERVER: successful GET');
+  res.send(JSON.stringify(obj));
+};
+
+exports.saveFavorite = function(req, res) {
+  console.log('request body', req.body);
+  res.send('SERVER: successful POST');
+};
--- server/server-config.js
+++ server/server-config.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var handler = require('./config/request-handler.js');
+// var bodyParser = require('body-parser');
 
 var app = express();
 
-app.get('/', handler.helloWorld);
+// var clientPath = express.static(__dirname + '/client');
+
+// app.use(bodyParser.json());
+app.use(express.static('client'));
+
+
+app.get('/hello', handler.helloWorld);
+app.get('/yelp', handler.requestYelp);
+app.post('/yelp', handler.saveFavorite);
 
 module.exports = app;
